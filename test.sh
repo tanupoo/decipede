c183fce8b38379f867f818ab2790c8ee44a4cc85
Shiochi Sakane <sakane@tanu.org>
2016-04-03 23:36:35 +0900

    passing a text in foreever into decipede.

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+(
 while true
 do
-	echo 0123456789012345678901234567890123456789 | ./decipede -C con
+	echo 0123456789012345678901234567890123456789
 	sleep 1
 done
+) | ./decipede -C -x con 
